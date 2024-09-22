7d58a47216f2171cfd6bb5330b8ed44bcbed4287
PhamNgoc04 <ngocpb04@gmail.com>
2024-09-22 19:24:08 +0700

    maddcntt

--- form_screen_DN/app.js
+++ form_screen_DN/app.js
@@ -1,28 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var CORRECT_USER = 'phamngoc04';
-    var CORRECT_PASS = '123';
+document.addEventListener("DOMContentLoaded", function () {
+    var CORRECT_USER = "phamngoc04";
+    var CORRECT_PASS = "123";
 
-    var inputUsername = document.getElementById('username');
-    var inputPassword = document.getElementById('password');
-    var formLogin = document.getElementById('loginForm');
-    var divErrors = document.getElementById('errors');
+    var inputUsername = document.getElementById("username");
+    var inputPassword = document.getElementById("password");
+    var formLogin = document.getElementById("loginForm");
+    var divErrors = document.getElementById("errors");
+    var errorsUser = document.querySelector(".error-username");
+    var errorsPassword = document.querySelector(".error-password");
 
-    // Đảm bảo các ô nhập liệu trống khi trang được tải
-    inputUsername.value = '';
-    inputPassword.value = '';
+    inputUsername.value = "";
+    inputPassword.value = "";
 
-    // Xử lý sự kiện submit form
-    formLogin.addEventListener('submit', function (e) {
-        e.preventDefault(); // Ngăn hành vi submit mặc định
-
+    formLogin.addEventListener("submit", function (e) {
+        e.preventDefault();
+        errorsUser.innerText = "";
+        errorsPassword.innerText = "";
         var username = inputUsername.value.trim();
         var password = inputPassword.value.trim();
 
-        // Kiểm tra thông tin đăng nhập
         if (username === CORRECT_USER && password === CORRECT_PASS) {
-            window.location.href = 'https://www.youtube.com/watch?v=tlULZwdhFG0';
+            window.location.href = "https://getbootstrap.com/";
         } else {
-            // Thêm dấu nháy bao quanh HTML
+            if (username !== CORRECT_USER) {
+                errorsUser.innerText = "Sai tài khoản";
+            } else {
+                errorsUser.innerText = "";
+            }
+            if (password !== CORRECT_PASS) {
+                errorsPassword.innerText = "Sai mật khẩu";
+            } else {
+                errorsPassword.innerText = "";
+            }
+
             divErrors.innerHTML = `
                 <div class="alert alert-danger">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
@@ -31,8 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
             `;
         }
 
-        // // Làm mới các ô nhập liệu sau khi xử lý
-        // inputUsername.value = '';
-        inputPassword.value = '';
+        inputPassword.value = "";
     });
 });
